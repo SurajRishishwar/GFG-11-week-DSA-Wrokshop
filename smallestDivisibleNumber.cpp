fc2cefa39d2d2dcc833456d0a72f1f98ce5dd90f
SurajRishishwar <75289779+SurajRishishwar@users.noreply.github.com>
2020-12-02 11:01:50 +0530

    Create smallestDivisibleNumber.cpp

---
+++ smallestDivisibleNumber.cpp
@@ -0,0 +1,13 @@
+//This is Only Function 
+
+long long getSmallestDivNum(long long n)
+{
+      long long ans=1;
+      for(long long i=1;i<=n;i++)
+      {
+         ans=(ans*i)/(__gcd(ans,i));
+      }
+      
+      return ans;
+    
+    }
