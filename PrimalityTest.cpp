c6be19c70de2cda32ee2cb5caff42d6c3fdfc15a
SurajRishishwar <75289779+SurajRishishwar@users.noreply.github.com>
2020-12-02 11:22:33 +0530

    Create PrimalityTest.cpp

---
+++ PrimalityTest.cpp
@@ -0,0 +1,18 @@
+//This is Only Function
+
+bool isPrime(int N)
+{
+     if(N == 1) 
+     {
+        return false; 
+     }
+     
+    for (int i=2; i<N; i++)
+    {
+        if (N%i == 0)
+        { 
+           return false; 
+        }
+    }
+    return true; 
+}
