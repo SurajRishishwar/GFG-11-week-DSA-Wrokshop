38cafc537fd05c108da46acb4993e4f955862c4b
SurajRishishwar <75289779+SurajRishishwar@users.noreply.github.com>
2021-02-02 22:11:43 +0530

    Create MergeSort.cpp

---
+++ MergeSort.cpp
@@ -0,0 +1,45 @@
+//This is only function
+
+void merge(int arr[], int l, int m, int r)
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    int L[n1], R[n2];
+ 
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    int i = 0;
+ 
+    int j = 0;
+ 
+
+    int k = l;
+ 
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
